768cdcff655dfffebdc48999c779382ecb610e69
Mark Aperstein <mark.apartsin@gmail.com>
2022-10-08 16:30:48 +0300

    initial commit

---
+++ src/components/footbar.py
@@ -0,0 +1,15 @@
+from dash import html
+import dash_bootstrap_components as dbc
+
+footer = html.Footer(
+    dbc.Container(
+        [
+            html.Hr(),
+            'Footer item 1',
+            html.Br(),
+            'Footer item 2',
+            html.Br(),
+            'Footer item 3'
+        ]
+    )
+)
